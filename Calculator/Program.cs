9d2bc51d939dc64662f34ed7650b8d0fc4dde4f7
[Sadigkhan Gaykhanov] <[sadigkhaneg@code.edu.az]>
2021-12-13 23:45:09 +0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿using ClassLibrary1;
+
+CalCulator c=new CalCulator();
+Console.WriteLine($"Daxil etdiyiniz reqemlerin ceminden alinan netice:{c.Cem(5, 6)}");
+Console.WriteLine($"Daxil etsiyinz reqemin ozu ile eyni dereceden quvvete yukseldilmesinden alinan netice:{c.Quvvet(5)}");
+c.Muqayise(5,7);
+Console.WriteLine($"Daxil edilmis reqemlerin hasilinden alinmis netice:{c.Hasil(124, 1250)}");
+Console.WriteLine($"Daxil edilmis iki reqemin nisbetinden alinmis netice:{c.Bolme(234,12)}");
+Console.WriteLine($"Daxil edilmis ededlerin ferqinin tapilmasindan alinan netice:{c.Ferq(1243123,132123)}");
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,48 @@
+﻿namespace ClassLibrary1
+{
+    
+    
+        public class CalCulator
+        {
+            // Ededin quvvete ozu qeder quvvete yukseldilmesi   
+            public double Quvvet (double  reqem)
+            {
+                return reqem* reqem;
+            } 
+            // Daxil edilmis ededlerin ceminin  tapilmasi
+            public double Cem (double reqem1, double reqem2)
+            {
+                return reqem1 + reqem2;
+            }
+
+            // Daxil edilmis ededlerin hasilinin tapilmasi  
+            public double Hasil (double reqem1, double reqem2)
+            {
+                return reqem1 * reqem2;
+            }
+            // Daxil edilmis ededlerin muqayisesi 
+            public void Muqayise (double reqem1, double reqem2)
+            {
+                if (reqem1 > reqem2)
+                {
+                Console.WriteLine($"Daxil etdiyiniz reqemlerden boyuk olani:{reqem1}");
+                }
+                else
+                {
+                 Console.WriteLine($"Daxil etdiyiniz reqemlerdn boyuk olani:{reqem2}");
+                }                             
+            }
+            // Daxil edilmis ededlerin nisbetinin tapilmasi  
+            public float Bolme (float reqem1, float reqem2)
+            {
+                return reqem1 / reqem2;
+            }
+            // Daxil edilmis ededlerinn ferqinin tapilmasi
+            public double Ferq (double reqem1, double reqem2)
+            {
+            return reqem1 - reqem2;
+            }
+
+    }
+    
+}
